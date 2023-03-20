0c9086ff9ef29751a7d379d191dac1856adc38a1
Manuel Luján Vilchez <miveritas.vivir@gmail.com>
2023-03-21 00:45:53 +0100

    added link to db for tasks

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+import db
+from models import Tasks
+
 
 app = Flask(__name__)  # En app se encuentra nuestro servidor web de Flask
 
@@ -8,9 +11,15 @@ def home_web():
     return render_template("index.html")
 
 
+@app.route("/create_task", methods=["POST"])
+def create():
+    task = Tasks(content=request.form["content_task"], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return "Tarea guardada"
+
 if __name__ == "__main__":
+    db.Base.metadata.create_all(db.engine)
     app.run(debug=True)
     # app.run(debug=True, port=1111) Podemos elegir el puerto desde el que trabajar. Cuidado porque
     # hay puertos en uso por el pc y hay que comprobar que este libre.
-
-
