34e06cd24e8e27ab0930bf316fad8a7bbc706385
Manuel Luján Vilchez <miveritas.vivir@gmail.com>
2023-03-23 02:19:52 +0100

    add comments

--- db.py
+++ db.py
@@ -1,19 +1,25 @@
-# Engine. El engine permite a SQLAlchemy comunicarse con la Base de datos
+# Motor. El motor permite a SQLAlchemy comunicarse con la base de datos.
 # https://docs.sqlalchemy.org/en/14/core/engines.html
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Creamos el motor y especificamos que trabajaremos con una base de datos SQLite.
+# También le indicamos a la conexión que permita trabajar en diferentes hilos (threads).
 engine = create_engine('sqlite:///database/tasks.db', connect_args={"check_same_thread": False})
-# connect_args indica que las fuciones en segundo plano para que las funciones no tenga problemas con el html
+# connect_args indica que las funciones en segundo plano funcionen sin problemas con el HTML.
 connect_args = {'check_same_thread': False}
-# Advertencia, crear el engine no conecta inmediatamente a la base de datos eso lo hacemos más adelante
 
-# Ahora creamos la sesión lo que nos permite realizar transacciones (operaciones) dentro de nuestra BD
+# ¡Ojo! La creación del motor no nos conecta inmediatamente a la base de datos.
+# Esto lo haremos más adelante.
+
+# Ahora creamos la sesión, lo que nos permite realizar transacciones (operaciones) dentro de nuestra base de datos.
+# De esta forma, podemos consultar, crear, actualizar o eliminar registros.
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Indicarle al ORM que su trabajo principal va a ser transformar clases en tablas
-# Ahora vamos al fichero models.poy y en los modelos (clases) donde queremos que se transformen en tablas,
-# le añadiremos esta variable, y esto se encargara de mapear y vincular la clase a la tabla
+# Indicamos al ORM que su trabajo principal será transformar clases en tablas.
+# Vamos al archivo models.py y en las clases que queremos transformar en tablas,
+# agregamos esta variable. Esto se encargará de mapear y vincular la clase a la tabla.
 Base = declarative_base()
+
 
--- models.py
+++ models.py
@@ -1,39 +1,26 @@
 from sqlalchemy import Column, Integer, String, Boolean, Date
-from werkzeug.security import generate_password_hash, check_password_hash
 import db
 
-
+# Esta clase define el modelo de datos de nuestra tabla 'tasks' que utilizaremos en nuestro servidor Flask.
+# La clase hereda de la clase Base de SQLAlchemy que a su vez hereda de la clase declarative_base.
+# Cada instancia de esta clase representa una fila de nuestra tabla 'tasks'.
 class Tasks(db.Base):
-    __tablename__ = "tasks"
-    __table_args__ = {'sqlite_autoincrement': True}
-    id_task = Column(Integer, primary_key=True)
-    content = Column(String(200), nullable=False)
-    done = Column(Boolean)
-    category = Column(String(50), nullable=False)
-    due_date = Column(Date, nullable=False)
+    __tablename__ = "tasks" # Este atributo indica el nombre de la tabla en nuestra base de datos.
+    __table_args__ = {'sqlite_autoincrement': True} # Este atributo indica que la clave primaria se autoincrementa.
+    id_task = Column(Integer, primary_key=True) # Este atributo indica el número de tarea. Es nuestra clave primaria.
+    content = Column(String(200), nullable=False) # Este atributo indica el contenido de la tarea.
+    done = Column(Boolean) # Este atributo indica si la tarea ha sido completada o no.
+    category = Column(String(50), nullable=False) # Este atributo indica la categoría de la tarea.
+    due_date = Column(Date, nullable=False) # Este atributo indica la fecha límite de la tarea.
 
-class Access(db.Base):
-    __tablename__="access"
-    __table_args___ = {"sqlite_autoincrement": True}
-    id_access = Column(Integer, primary_key = True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(50), nullable=False)
-
-    def __init__(self, content, done, category, due_date, username, password):
+    # Este método constructor define cómo se crea una tarea.
+    def __init__(self, content, done, category, due_date):
         self.content = content
         self.done = done
         self.category = category
         self.due_date = due_date
-        self.username = username
-        self.password = password
 
-    def check_password(self):
-        return f'<User {self.username}>'
-
-    def __repr__(self):
-        return check_password_hash(self.password, password)
-
+    # Este método define cómo se representa una tarea como una cadena de caracteres.
     def __str__(self):
         return "Tarea: Nº {} = {} -> {})".format(self.id_task, self.content, self.done)
-
 
