b4b98676c1a50d9a628e0d2968e43d10272c643b
Manuel Luján Vilchez <miveritas.vivir@gmail.com>
2023-03-23 01:50:24 +0100

    add edit function, html and button

--- main.py
+++ main.py
@@ -43,6 +43,28 @@ def tasks_by_category(category):
     category_tasks = db.session.query(Tasks).filter_by(category=category).all()
     return render_template("index.html", task_list=category_tasks)
 
+@app.route("/edit-task/<id>", methods=["GET", "POST"])
+def edit(id):
+    task = db.session.query(Tasks).filter_by(id_task=id).first()
+    if request.method == "POST":
+        task.content = request.form["content_task"]
+        task.category = request.form["category"]
+        task.due_date = datetime.strptime(request.form["due_date"], "%Y-%m-%d").date()
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home_web"))
+    else:
+        return render_template("edit.html", task=task)
+
+@app.route("/update_task/<int:id>", methods=["POST"])
+def update(id):
+    task = db.session.query(Tasks).filter_by(id_task=id).first()
+    task.content = request.form["content_task"]
+    task.category = request.form["category"]
+    task.due_date = datetime.datetime.strptime(request.form["due_date"], "%Y-%m-%d").date()
+    db.session.commit()
+    return redirect(url_for("home_web"))
+
 if __name__ == "__main__":
     db.Base.metadata.create_all(db.engine)
     app.run(debug=True)
